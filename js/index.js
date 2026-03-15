94c05e3dc74b9c46427f1c52973be8c989dc8dbf
ppopeta <bibianacortes@unicauca.edu.co>
2026-03-15 16:59:00 -0500

    se agrega secion de link en el index + se arrreglan fotos y carrusel

--- js/index.js
+++ js/index.js
@@ -1,11 +1,64 @@
 console.log("Efectivamente el JS esta cargando!");
 
-const slider = document.getElementById("slider");
+const carrusel = document.getElementById("carrusel");
 
-slider.addEventListener("wheel", (event) => {
+document.querySelector(".izquierda").addEventListener("click", () => {
+    carrusel.scrollBy({
+        left: -400,
+        behavior: "smooth"
+    });
+});
 
-    event.preventDefault();
+document.querySelector(".derecha").addEventListener("click", () => {
+    carrusel.scrollBy({
+        left: 400,
+        behavior: "smooth"
+    });
+});
 
-    slider.scrollLeft += event.deltaY;
+const links = [
+    {
+    nombre: "GitHub",
+    url: "https://github.com/ppopeta"
+    },
+    {
+    nombre: "Steam",
+    url: "https://steamcommunity.com/profiles/76561199442800009/"
+    },
+    {
+    nombre: "Correo",
+    tipo: "correo",
+    email: "bibianacortes@unicauca.edu.co"
+    },
+    {
+    nombre: "Lichess",
+    url: "https://lichess.org/@/ppopetaa"
+    }
+];
 
+const contenedor = document.getElementById("links-caja");
+
+links.forEach(link => {
+
+const enlace = document.createElement("a");
+
+enlace.textContent = link.nombre;
+enlace.classList.add("link-item");
+
+if(link.tipo === "correo"){
+    enlace.href = "#";
+    enlace.addEventListener("click", (e) => {
+        e.preventDefault();
+        navigator.clipboard.writeText(link.email);
+        const mensaje = document.getElementById("mensaje-copiado");
+        mensaje.classList.add("mostrar");
+        setTimeout(() => {
+            mensaje.classList.remove("mostrar");
+        }, 2000);
+    });
+}else{
+    enlace.href = link.url;
+    enlace.target = "_blank";
+}
+contenedor.appendChild(enlace);
 });
